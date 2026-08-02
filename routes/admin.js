2c0280f585ea741760755f2332cf483df39139c2
Heroblack1 <danjumahero@gmail.com>
2026-08-02 21:18:44 +0100

    deployment

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const Contact = require("../models/Contact");
+const Contact = require("../models/contact");
 
 const router = express.Router();
 
